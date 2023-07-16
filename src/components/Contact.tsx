1bc94912ef978c042f60cde2eb3fcfa6ae8a2be5
Angel116 <a_flores116@yahoo.com>
2023-07-16 17:38:41 -0500

    - edits to submission message and styling of the form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import React, {useState}from 'react'
 import BackgroundVideoHero from './BackgroundVideoHero'
 import CorgiVideo2 from '../assets/video/corgi-video-2.mp4'
 import ContactForm from './ContactForm';
+import Divider from './Divider';
 
 
      
@@ -27,9 +28,17 @@ const Contact = () => {
             isPageTitle={false}
         />
                     <div className="container">
-                        {!submitted ? ( <ContactForm onSubmit={handleSubmit}/>) : (<div>thank you for contacting us!</div>)}
+                      <Divider
+                        dividerTitle={"Contact Us"} 
+                      />
+                      <h3>Have any questions about corgis? Fill out the form.</h3>
+                        {!submitted ? ( <ContactForm onSubmit={handleSubmit}/>) : 
+                        (<div className='form-submission-message'>
+                          <p>Thank you for contacting us, unfortunately this is a project site and this form is only for brushing up my skills. Essentially, this form doesn't work. No message was sent. 😈 </p>
+                        </div>
+                          )
+                        }
                     </div>
-                    
 
     </>
   )
